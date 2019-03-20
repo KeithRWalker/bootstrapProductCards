2e20c3564a82dbfd1a47fae8d5ab8921d4f492b6
Keith Walker <Keithrwalker418@gmail.com>
2019-03-19 21:31:18 -0500

    wip

--- main.js
+++ main.js
@@ -1,1 +1,152 @@
-console.log('main.js linked')
+const products = [
+    {
+        title: 'product1',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut cursus mollis ligula, id sollicitudin justo. Maecenas in placerat ipsum, id tempus nulla. Sed ultricies ipsum in nisi posuere dictum. Phasellus rutrum ac dolor tincidunt dignissim. Proin eget interdum nunc, et venenatis nisi. Morbi et felis enim. Quisque sit amet congue ligula. Donec at congue neque. Vestibulum vel faucibus diam. Fusce non mauris gravida, pulvinar erat in, malesuada turpis.',
+        imgUrl: 'https://picsum.photos/150/150/?random',
+        size: '4x4',
+        weight: '8 oz.',
+        price1: '$2.00',
+        price2: '$1.50',
+        price3: '$1.00',
+        validUntill: '04/18/2019'
+    },
+    {
+        title: 'product2',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut cursus mollis ligula, id sollicitudin justo. Maecenas in placerat ipsum, id tempus nulla. Sed ultricies ipsum in nisi posuere dictum. Phasellus rutrum ac dolor tincidunt dignissim. Proin eget interdum nunc, et venenatis nisi. Morbi et felis enim. Quisque sit amet congue ligula. Donec at congue neque. Vestibulum vel faucibus diam. Fusce non mauris gravida, pulvinar erat in, malesuada turpis.',
+        imgUrl: 'https://picsum.photos/150/150/?random',
+        size: '4x4',
+        weight: '8 oz.',
+        price1: '$2.00',
+        price2: '$1.50',
+        price3: '$1.00',
+        validUntill: '04/18/2019'
+    },
+    {
+        title: 'product3',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut cursus mollis ligula, id sollicitudin justo. Maecenas in placerat ipsum, id tempus nulla. Sed ultricies ipsum in nisi posuere dictum. Phasellus rutrum ac dolor tincidunt dignissim. Proin eget interdum nunc, et venenatis nisi. Morbi et felis enim. Quisque sit amet congue ligula. Donec at congue neque. Vestibulum vel faucibus diam. Fusce non mauris gravida, pulvinar erat in, malesuada turpis.',
+        imgUrl: 'https://picsum.photos/150/150/?random',
+        size: '4x4',
+        weight: '8 oz.',
+        price1: '$2.00',
+        price2: '$1.50',
+        price3: '$1.00',
+        validUntill: '04/18/2019'
+    },
+    {
+        title: 'product4',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut cursus mollis ligula, id sollicitudin justo. Maecenas in placerat ipsum, id tempus nulla. Sed ultricies ipsum in nisi posuere dictum. Phasellus rutrum ac dolor tincidunt dignissim. Proin eget interdum nunc, et venenatis nisi. Morbi et felis enim. Quisque sit amet congue ligula. Donec at congue neque. Vestibulum vel faucibus diam. Fusce non mauris gravida, pulvinar erat in, malesuada turpis.',
+        imgUrl: 'https://picsum.photos/150/150/?random',
+        size: '4x4',
+        weight: '8 oz.',
+        price1: '$2.00',
+        price2: '$1.50',
+        price3: '$1.00',
+        validUntill: '04/18/2019'
+    },
+    {
+        title: 'product5',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut cursus mollis ligula, id sollicitudin justo. Maecenas in placerat ipsum, id tempus nulla. Sed ultricies ipsum in nisi posuere dictum. Phasellus rutrum ac dolor tincidunt dignissim. Proin eget interdum nunc, et venenatis nisi. Morbi et felis enim. Quisque sit amet congue ligula. Donec at congue neque. Vestibulum vel faucibus diam. Fusce non mauris gravida, pulvinar erat in, malesuada turpis.',
+        imgUrl: 'https://picsum.photos/150/150/?random',
+        size: '4x4',
+        weight: '8 oz.',
+        price1: '$2.00',
+        price2: '$1.50',
+        price3: '$1.00',
+        validUntill: '04/18/2019'
+    },
+    {
+        title: 'product6',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut cursus mollis ligula, id sollicitudin justo. Maecenas in placerat ipsum, id tempus nulla. Sed ultricies ipsum in nisi posuere dictum. Phasellus rutrum ac dolor tincidunt dignissim. Proin eget interdum nunc, et venenatis nisi. Morbi et felis enim. Quisque sit amet congue ligula. Donec at congue neque. Vestibulum vel faucibus diam. Fusce non mauris gravida, pulvinar erat in, malesuada turpis.',
+        imgUrl: 'https://picsum.photos/150/150/?random',
+        size: '4x4',
+        weight: '8 oz.',
+        price1: '$2.00',
+        price2: '$1.50',
+        price3: '$1.00',
+        validUntill: '04/18/2019'
+    },
+    {
+        title: 'product7',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut cursus mollis ligula, id sollicitudin justo. Maecenas in placerat ipsum, id tempus nulla. Sed ultricies ipsum in nisi posuere dictum. Phasellus rutrum ac dolor tincidunt dignissim. Proin eget interdum nunc, et venenatis nisi. Morbi et felis enim. Quisque sit amet congue ligula. Donec at congue neque. Vestibulum vel faucibus diam. Fusce non mauris gravida, pulvinar erat in, malesuada turpis.',
+        imgUrl: 'https://picsum.photos/150/150/?random',
+        size: '4x4',
+        weight: '8 oz.',
+        price1: '$2.00',
+        price2: '$1.50',
+        price3: '$1.00',
+        validUntill: '04/18/2019'
+    },
+    {
+        title: 'product8',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut cursus mollis ligula, id sollicitudin justo. Maecenas in placerat ipsum, id tempus nulla. Sed ultricies ipsum in nisi posuere dictum. Phasellus rutrum ac dolor tincidunt dignissim. Proin eget interdum nunc, et venenatis nisi. Morbi et felis enim. Quisque sit amet congue ligula. Donec at congue neque. Vestibulum vel faucibus diam. Fusce non mauris gravida, pulvinar erat in, malesuada turpis.',
+        imgUrl: 'https://picsum.photos/150/150/?random',
+        size: '4x4',
+        weight: '8 oz.',
+        price1: '$2.00',
+        price2: '$1.50',
+        price3: '$1.00',
+        validUntill: '04/18/2019'
+    },
+    {
+        title: 'product9',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut cursus mollis ligula, id sollicitudin justo. Maecenas in placerat ipsum, id tempus nulla. Sed ultricies ipsum in nisi posuere dictum. Phasellus rutrum ac dolor tincidunt dignissim. Proin eget interdum nunc, et venenatis nisi. Morbi et felis enim. Quisque sit amet congue ligula. Donec at congue neque. Vestibulum vel faucibus diam. Fusce non mauris gravida, pulvinar erat in, malesuada turpis.',
+        imgUrl: 'https://picsum.photos/150/150/?random',
+        size: '4x4',
+        weight: '8 oz.',
+        price1: '$2.00',
+        price2: '$1.50',
+        price3: '$1.00',
+        validUntill: '04/18/2019'
+    },
+    {
+        title: 'product10',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut cursus mollis ligula, id sollicitudin justo. Maecenas in placerat ipsum, id tempus nulla. Sed ultricies ipsum in nisi posuere dictum. Phasellus rutrum ac dolor tincidunt dignissim. Proin eget interdum nunc, et venenatis nisi. Morbi et felis enim. Quisque sit amet congue ligula. Donec at congue neque. Vestibulum vel faucibus diam. Fusce non mauris gravida, pulvinar erat in, malesuada turpis.',
+        imgUrl: 'https://picsum.photos/150/150/?random',
+        size: '4x4',
+        weight: '8 oz.',
+        price1: '$2.00',
+        price2: '$1.50',
+        price3: '$1.00',
+        validUntill: '04/18/2019'
+    }
+];
+
+const init = () =>{
+    arrayLoop(products);
+};
+
+const printToDom = (id, text) => {
+    const selectedDiv = document.getElementById(id);
+    selectedDiv.innerHTML = text;
+};
+
+const arrayLoop = (yourArray) => {
+    let printString = ``; 
+    // printString += `<div class="container cardCon">`;
+    printString += `<div class="row">`;
+
+    yourArray.forEach ((e) => {
+
+        printString += `<div class= "col-4 product">`;
+        printString +=  `<div class="card">`
+        printString +=      `<h5 class="card-header">${e.title}</h5>`;
+        printString +=      `<img src="${e.imgUrl}" class="card-img-top" alt="Image of a ${e.title}">`;
+        printString +=      `<div class="card-body">`;
+        printString +=          `<p class="card-text">${e.description}</p>`;
+        printString +=          `<div class="card-title avalible">avalible</div>`;
+        printString +=          `<h4>pricing</h4>`
+        printString +=          `<ul class="list-group">`
+        printString +=              `<li class="list-group-item">1: ${e.price1}</li>`
+        printString +=              `<li class="list-group-item">5-10: ${e.price2}</li>`
+        printString +=              `<li class="list-group-item">10 or more: ${e.price3}</li>`
+        printString +=          `</ul>`
+        printString +=      `</div>`;
+        printString +=  `</div>`;
+        printString += `</div>`;
+        
+    })
+    printString += `</div>`
+    printToDom('printDiv', printString);
+}
+
+init();
